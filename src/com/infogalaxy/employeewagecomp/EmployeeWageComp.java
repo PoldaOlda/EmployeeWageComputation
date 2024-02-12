73726a11cf0002270ccd7a0b0a497c623ebe19fb
PoldaOlda <poldaolda123@gmail.com>
2024-02-12 11:08:27 +0530

    [MODIFIED]:Tahir - UC-1 Completed for Employee Attendance

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -1,10 +1,24 @@
 package com.infogalaxy.employeewagecomp;
 
+import java.util.Scanner;
+
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int attendance = 0;
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		System.out.println("Enter the Attendance: [0-Absent / 1-Present]:");
+		attendance = sc.nextInt();
+		
+		if(attendance == 0) {
+			System.out.println("Employee is Absent");
+		}
+		else {
+			System.out.println("Employee is Present");
+		}
 	}
 
 }
