4610050e6f0b66cebde51a7e911ba7b59db10b11
PoldaOlda <poldaolda123@gmail.com>
2024-02-13 11:00:28 +0530

    [MODIFIED]:Tahir - Calculated Daily Wage if Employee Present

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -7,6 +7,10 @@ public class EmployeeWageComp {
 	public static void main(String[] args) {
 		
 		int attendance = 0;
+		int dailyWage = 0;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
 		Random random = new Random();
@@ -17,6 +21,8 @@ public class EmployeeWageComp {
 		}
 		else {
 			System.out.println("Employee is Present");
+			dailyWage = WAGE_PER_HR * FULL_DAY_HR;
+			System.out.println("Employee Wage = "+ dailyWage);
 		}
 	}
 
