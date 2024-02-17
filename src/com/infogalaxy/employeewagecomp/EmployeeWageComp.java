cb7b05628a65385ed010d0d8706d7e1afab239a9
PoldaOlda <poldaolda123@gmail.com>
2024-02-17 11:00:34 +0530

    [MODIFIED]:Tahir - Refactored code from if-else to switch-case

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -8,7 +8,7 @@ public class EmployeeWageComp {
 		
 		int attendance;
 		int employee_type;
-		int dailyWage;
+		int dailyWage = 0;
 		final int WAGE_PER_HR = 20;
 		final int FULL_TIME_HR = 8;
 		final int PART_TIME_HR = 4;
@@ -18,20 +18,25 @@ public class EmployeeWageComp {
 		Random random = new Random();
 		attendance = random.nextInt(9) % 2;
 		
-		if(attendance == 0) {
+		switch(attendance) {
+		case 0:
 			System.out.println("Employee is Absent");
-		}
-		else {
+			break;
+		case 1:
 			System.out.println("Employee is Present");
 			employee_type = random.nextInt(9) % 2;
-			if(employee_type == 0) {
+			switch(employee_type) {
+			case 0:
 				System.out.println("Employee is Part Time");
 				dailyWage = WAGE_PER_HR * PART_TIME_HR;
-			} else {
+				break;
+			case 1:
 				System.out.println("Employee is Full Time");
 				dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+				break; 
 			}
 			System.out.println("Employee Wage = "+ dailyWage);
+			break;
 		}
 	}
 
