fe61d58abbb05f42007f766b821653fe1d28c996
PoldaOlda <poldaolda123@gmail.com>
2024-02-21 10:58:02 +0530

    [MODIFIED]:Tahir - Defined code for Multiple Companies

--- src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomp/EmployeeWageComp.java
@@ -1,6 +1,7 @@
 package com.infogalaxy.employeewagecomp;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class EmployeeWageComp {
 
@@ -10,15 +11,27 @@ public class EmployeeWageComp {
 	int total_wage = 0;
 	int total_working_hr = 0;
 	int total_working_days = 1;
-	final int WAGE_PER_HR = 20;
+	int wage_per_hr;
 	final int FULL_TIME_HR = 8;
 	final int PART_TIME_HR = 4;
-	final int WORKING_DAYS = 20;
+	int working_days;
+	int working_hr_month;
+	
+//	public void companyInput() {
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("Enter the Wage Per Hours:");
+//		wage_per_hr = scanner.nextInt();
+//		System.out.println("Enter the Working Days in Month:");
+//		Working_days = scanner.nextInt();
+//		System.out.println("Enter the Working Hours per MOnth:");
+//		working_hr_month = scanner.nextInt();
+//	}
+
 	
 	public void employeeWageCalc(){
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
-		while(total_working_hr <= 100 && total_working_days <= 20) {
+		while(total_working_hr <= working_hr_month && total_working_days <= working_days) {
 			
 			System.out.println("Day="+total_working_days);
 			Random random = new Random();
@@ -34,12 +47,12 @@ public class EmployeeWageComp {
 				switch(employee_type) {
 				case 0:
 					System.out.println("Employee is Part Time");
-					dailyWage = WAGE_PER_HR * PART_TIME_HR;
+					dailyWage = wage_per_hr * PART_TIME_HR;
 					total_working_hr += PART_TIME_HR;
 					break;
 				case 1:
 					System.out.println("Employee is Full Time");
-					dailyWage = WAGE_PER_HR * FULL_TIME_HR;
+					dailyWage = wage_per_hr * FULL_TIME_HR;
 					total_working_hr += FULL_TIME_HR;
 					break; 
 				}
@@ -54,9 +67,17 @@ public class EmployeeWageComp {
 		System.out.println("Total Monthly Wage="+total_wage);
 	}
 
+	public EmployeeWageComp(int wage_per_hr, int working_days,int working_hr_month) {
+		this.wage_per_hr = wage_per_hr;
+		this.working_days = working_days;
+		this.working_hr_month = working_hr_month;
+	}
+
 	public static void main(String[] args) {
-		EmployeeWageComp employeeWageComp = new EmployeeWageComp();
+		EmployeeWageComp employeeWageComp = new EmployeeWageComp(22,18,80);
 		employeeWageComp.employeeWageCalc();
+		EmployeeWageComp employeeWageComp2 = new EmployeeWageComp(15, 22, 120);
+		employeeWageComp2.employeeWageCalc();
 	}
 
 }
